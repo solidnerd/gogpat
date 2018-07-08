cecafb6e5ab44874f708600a9403e6029fdf73a7
solidnerd <niclas@mietz.io>
2018-07-08 15:14:27 +0200

    Add GitLab versions 11 and 10.X for testing
    Signed-off-by: solidnerd <niclas@mietz.io>

--- ci/test.sh
+++ ci/test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash 
 set -eu
 PKG_LIST=$1
-pushd ci
-docker-compose up -d
 
-until curl -sSf http://localhost:10080/users/sign_in  > /dev/null
-do
-  sleep 10
+versions=( \
+  10.2.5 \
+  10.8.4 \
+  11.0.2 \
+)
+
+for version in "${versions[@]}"; do
+  pushd ci
+  printf "Testing with GitLab $version\n\n"
+  GITLAB_VERSION=${version} docker-compose up -d
+  until curl -sSf http://localhost:10080/explore  > /dev/null
+  do
+    sleep 10
+  done
+  popd
+  printf "Testing...\n\n"
+  go test -v -covermode=count -coverprofile=profile.cov -timeout=1200s ${PKG_LIST}
+  pushd ci
+  printf "Teardown...\n\n"
+  GITLAB_VERSION=${version} docker-compose down -v --rmi 'local'
+  popd
 done
-popd 
-go test -v -covermode=count -coverprofile=profile.cov -timeout=1200s ${PKG_LIST}
-pushd ci
-docker-compose down -v
-popd
+
